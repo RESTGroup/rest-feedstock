077c5a7c45b92f900ec1ae426cc4bdafe447745a
Shirong Wang <srwang20@fudan.edu.cn>
2026-04-05 23:44:37 +0800

    Disable error handling in run_tests.sh
    Comment out the error handling for rest_regression.

--- recipe_rattler/run_tests.sh
+++ recipe_rattler/run_tests.sh
@@ -7,9 +7,10 @@ which rest
 echo ${REST_EXT_DIR}
 echo ${REST_HOME}
 echo ${PREFIX}
+${PREFIX}/bin/rest_regression -r ./bench_pool -p ${PREFIX}/bin/rest
 # catch the error if rest_regression fail and print the log file
-if ! ${PREFIX}/bin/rest_regression -r ./bench_pool -p rest; then
-    cd bench_pool/CO_HF_Dipole
-    rest
-    exit 1
-fi
+# if ! ${PREFIX}/bin/rest_regression -r ./bench_pool -p ${PREFIX}/bin/rest; then
+#     cd bench_pool/CO_HF_Dipole
+#     rest
+#     exit 1
+# fi
