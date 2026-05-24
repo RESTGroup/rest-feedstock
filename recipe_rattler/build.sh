93edd98af2e5cff974a873c140aa7d047b01e3e2
Shirong_Wang <srwang20@fudan.edu.cn>
2026-05-24 16:56:27 +0800

    try ctng

--- recipe_rattler/build.sh
+++ recipe_rattler/build.sh
@@ -1,10 +1,31 @@
 set -ex
 
+cat > Cargo.toml << 'EOF'
+[workspace]
+resolver = "2"
+
+members = [
+    "rest_tensors",
+    "rest_libcint",
+    "rest",
+    "rest_regression",
+]
+
+[profile.release]
+opt-level = 3
+lto = "fat"
+codegen-units = 1
+EOF
+
 if [[ "$target_platform" == win-64 ]]; then
   LIB_EXT="dll"
   REST_EXT_DIR="${PREFIX}/Library/bin"
+  HDF5_DIR="${PREFIX}"
 
-  MOKIT_LIB="${BUILD_PREFIX}/Lib/site-packages/mokit/lib/librest2fch.${LIB_EXT}"
+  MOKIT_LIB="${PREFIX}/Lib/site-packages/mokit/lib/librest2fch.${LIB_EXT}"
+
+  export CONDA_BUILD_WINSDK="${BUILD_PREFIX}/x86_64-w64-mingw32/sysroot"
+  export CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER="${CC}"
 else
   if [[ "$target_platform" == osx-* ]]; then
     LIB_EXT="dylib"
@@ -13,6 +34,7 @@ else
   fi
 
   REST_EXT_DIR="${PREFIX}/lib"
+  HDF5_DIR="${BUILD_PREFIX}"
   MOKIT_LIB="${BUILD_PREFIX}/lib/python${PY_VER}/site-packages/mokit/lib/librest2fch.${LIB_EXT}"
 fi
 
@@ -25,6 +47,7 @@ if [[ ! -f "${MOKIT_LIB}" ]]; then
 fi
 cp "${MOKIT_LIB}" "${REST_EXT_DIR}/"
 
+export HDF5_DIR
 export LIBCLANG_PATH="${BUILD_PREFIX}/lib"
 export REST_EXT_DIR
 
